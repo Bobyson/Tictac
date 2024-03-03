96bc4469ca9b06a16b858497abd02f80c75fcdac
Bobyson <acntanouba@gmail.com>
2024-03-03 20:34:56 +0530

    Initial Commit

---
+++ Tic-Tac/src/App.jsx
@@ -0,0 +1,18 @@
+import Board from './components/Board'
+import './App.css'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Board/>
+      </div>
+    </>
+  )
+}
+
+export default App

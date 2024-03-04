bf88ffa666769929416cdd8d8d21c39c8afb42ed
Bobyson <acntanouba@gmail.com>
2024-03-04 12:26:39 +0530

    after style

--- Tic-Tac/src/components/Board.jsx
+++ Tic-Tac/src/components/Board.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Square from "./Square";
 import "./board.css";
 
@@ -14,10 +14,10 @@ function calculateWinner(squares) {
     [2, 4, 6],
   ];
 
-  for(let i = 0; i<lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+      return squares[a];
     }
   }
   return null;
@@ -27,9 +27,11 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const [status, setStatus] = useState("Start your game");
+
   const handleClick = (i) => {
     if (squares[i] || calculateWinner(squares)) {
-        return;
+      return;
     }
     if (squares[i]) {
       return;
@@ -45,31 +47,50 @@ function Board() {
     setXIsNext(!xIsNext);
   };
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = winner + " " + "won";
-  } else {
-    status = "Next player is"+ " " + (xIsNext? "X" : "O");
-  }
+  const handleReset = () => {
+    // resets the squares to null
+    setSquares(Array(9).fill(null));
+    // set to X
+    setXIsNext(true);
+  };
+
+  useEffect(() => {
+    console.log('useEffect triggered!')
+    const winner = calculateWinner(squares);
+    //   let status;
+    if (winner) {
+      setStatus(winner + " " + "won");
+    } else {
+      setStatus("Next player is" + " " + (xIsNext ? "X" : "O"));
+    }
+  }, [squares, xIsNext]);
 
   return (
     <>
+      <div className="container">
         <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <div className="board">
+          <div className="board-row">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
+        </div>
+        <div className="reset-board">
+          <button className="reset-btn" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
       </div>
     </>
   );
